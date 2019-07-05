cfba5fc80ffe0ec342046bbbc42d72cb1e540d06
Yeison Caicedo <yecaicedo@unicauca.edu.co>
2019-07-05 18:06:24 -0500

    register

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,11 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-//mix.js('resources/js/app.js', 'public/js')
-   //.sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
 
-mix.js('resources/js/appLanding.js', 'public/js')
-   .sass('resources/sass/appLanding.scss', 'public/css');   
+//mix.js('resources/js/appLanding.js', 'public/js')
+  // .sass('resources/sass/appLanding.scss', 'public/css');   
 
 mix.browserSync({
     proxy: '127.0.0.1:8000'
