77755e276dddaf7eaaac8d9e35863dafeb6c495d
Yeison Caicedo <yecaicedo@unicauca.edu.co>
2019-09-10 19:48:33 -0500

    estadisticas

--- resources/js/components/DashboardComponent.vue
+++ resources/js/components/DashboardComponent.vue
@@ -78,131 +78,93 @@
 
       
 
-        <div class="row">
-          <div class="col-12">
-            <div class="row">
-              <div class="col-12">
-                <h4>Estadísticas generales</h4>
-              </div>
-              <br />
-              <br />
-              <div class="col-12">
-                <h5>Ideas</h5>
-              </div>
-            </div>
-            <br />
-          </div>
-          <div
-            class="col-12"
-            style="  overflow: scroll;  background-color:#F7F8FB; box-shadow: 0 5px 5px rgba(0,0,0,0.05), 0 3px 3px rgba(0,0,0,0.05);"
-          >
-            <table class="table table-striped table-hover">
-              <thead class="thead-dark">
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Nombre</th>
-                  <th scope="col">Me gusta</th>
-                  <th scope="col">Pulir más</th>
-                  <th scope="col">Acción de mejora</th>
-
-                  <th scope="col">Total votos</th>
-                  <th scope="col">Comentarios</th>
-
-                  <th scope="col">Vistas</th>
-                </tr>
-              </thead>
-              <tbody style="font-size:15px; !important">
-                <tr v-for="(idea,index) in ideas" :key="idea.id">
-                  <th scope="row">{{(idea.id)}}</th>
-                  <td>{{idea.title}}</td>
-                  <td>{{idea.megusta}}</td>
-                  <td>{{idea.pulirmas}}</td>
-                  <td>{{idea.accion}}</td>
-
-                  <td>{{ idea.total}}</td>
-                  <td>{{idea.comentarios}}</td>
-                  <td>{{idea.vistas}}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
         <!-- Fin table ideas -->
 
         <br />
+              <div class="row">
+                <div class="col-12">
+                    <v-toolbar flat color="#CFD8DC">
+                    <v-toolbar-title>Ideas</v-toolbar-title>
+                   
+                  </v-toolbar>
+                  <v-data-table
+                    :headers="headersIdeas"
+                    :items="ideas"
+                    class="elevation-1"
+                    >
+                    <template v-slot:items="props">
+                      <td>{{ props.item.title }}</td>
+                
+                      <td class="text-xs-left">{{ props.item.megusta }}</td>
+                      <td class="text-xs-left">{{ props.item.pulirmas }}</td>
+                      <td class="text-xs-left">{{ props.item.accion }}</td>
+                      <td class="text-xs-left">{{ props.item.total }}</td>
+                      <td class="text-xs-left">{{ props.item.comentarios }}</td>
+                      <td class="text-xs-left">{{ props.item.vistas }}</td>
+                  
+                    </template>
+                  </v-data-table>
+                </div>
+              </div>
+    
 
-        <div class="row">
-          <h5>Retos</h5>
-          <br />
-          <br />
-          <div
-            class="col-12"
-            style="background-color:#F7F8FB; box-shadow: 0 5px 5px rgba(0,0,0,0.05), 0 3px 3px rgba(0,0,0,0.05);"
-          >
-            <table class="table table-striped table-hover">
-              <thead class="thead-dark">
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Nombre</th>
-                  <th scope="col">Comentarios</th>
-                  <th scope="col">Vistas</th>
-                  <th scope="col">Soluciones</th>
-                </tr>
-              </thead>
-              <tbody style="font-size:15px; !important">
-                <tr v-for="(reto) in retos" :key="reto.id">
-                  <th scope="row">{{(reto.id)}}</th>
-                  <td>{{reto.title}}</td>
-
-                  <td>{{reto.comentarios}}</td>
-                  <td>{{reto.vistas}}</td>
-                  <td>{{reto.soluciones}}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
         <!-- Fin tabla retos -->
 
         <br />
 
-        <div class="row">
-          <h5>Usuarios</h5>
-          <br />
-          <br />
-          <div
-            class="col-12"
-            style="background-color:#F7F8FB; box-shadow: 0 5px 5px rgba(0,0,0,0.05), 0 3px 3px rgba(0,0,0,0.05);"
-          >
-            <table class="table table-striped table-hover">
-              <thead class="thead-dark">
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Nombre</th>
-                  <th scope="col">Puntos</th>
-                  <th scope="col">Votos</th>
-                  <th scope="col">Comentarios</th>
-                  <th scope="col">Ingresos</th>
-                  <th scope="col">Ideas</th>
-                  <th scope="col">Retos</th>
-                </tr>
-              </thead>
-              <tbody style="font-size:15px; !important">
-                <tr v-for="user in usuarios" :key="user.id">
-                  <th scope="row">{{(user.id)}}</th>
-                  <td>{{user.nombre}}</td>
-                  <td>{{user.puntos}}</td>
-                  <td>{{user.votos}}</td>
-                  <td>{{user.comentarios}}</td>
-                  <td>{{user.ingresos}}</td>
-                  <td>{{user.ideas}}</td>
-                  <td>{{user.retos}}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <!-- Fin tabla usuarios -->
+            <div class="row">
+                <div class="col-12">
+                  <v-toolbar flat color="#CFD8DC">
+                    <v-toolbar-title>Retos</v-toolbar-title>
+                   
+                  </v-toolbar>
+                  <v-data-table
+                    :headers="headersRetos"
+                    :items="retos"
+                    class="elevation-1"
+                    >
+                    <template v-slot:items="props">
+                      <td>{{ props.item.title }}</td>
+                
+                      <td class="text-xs-left">{{ props.item.comentarios }}</td>
+                      <td class="text-xs-left">{{ props.item.vistas }}</td>
+                      <td class="text-xs-left">{{ props.item.soluciones }}</td>
+                  
+                    </template>
+                  </v-data-table>
+                </div>
+              </div>
+    
+
+
+        <br>
+
+
+              <div class="row">
+                <div class="col-12">
+                    <v-toolbar flat color="#CFD8DC">
+                    <v-toolbar-title>Usuarios</v-toolbar-title>
+                   
+                  </v-toolbar>
+                  <v-data-table
+                    :headers="headersUsuarios"
+                    :items="usuarios"
+                    class="elevation-1"
+                    >
+                    <template v-slot:items="props">
+                      <td>{{ props.item.nombre }}</td>
+                
+                      <td class="text-xs-left">{{ props.item.puntos }}</td>
+                      <td class="text-xs-left">{{ props.item.votos }}</td>
+                      <td class="text-xs-left">{{ props.item.comentarios }}</td>
+                      <td class="text-xs-left">{{ props.item.ingresos }}</td>
+                      <td class="text-xs-left">{{ props.item.ideas }}</td>
+                      <td class="text-xs-left">{{ props.item.retos }}</td>
+                    </template>
+                  </v-data-table>
+                </div>
+              </div>
+    
 
           </div>
       </div>
@@ -269,7 +231,50 @@ export default {
 
       ideas: {},
       retos: {},
-      usuarios: {}
+      usuarios: {},
+       headersIdeas: [
+          {
+            text: 'Nombre',
+            align: 'left',
+            sortable: false,
+            value: 'title'
+          },
+          { text: 'Me gusta', value: 'megusta' },
+          { text: 'Pulir más', value: 'pulirmas' },
+          { text: 'Acción de mejora', value: 'accion' },
+           { text: 'Total votos', value: 'total' },
+          { text: 'Comentarios', value: 'comentarios' },
+          { text: 'Vistas', value: 'vistas' },
+        
+        ],
+       headersRetos: [
+          {
+            text: 'Nombre',
+            align: 'left',
+            sortable: false,
+            value: 'title'
+          },
+          { text: 'Comentarios', value: 'comentarios' },
+          { text: 'Vistas', value: 'vistas' },
+          { text: 'Soluciones', value: 'soluciones' },
+        
+        ],
+      headersUsuarios: [
+          {
+            text: 'Nombre',
+            align: 'left',
+            sortable: false,
+            value: 'nombre'
+          },
+          { text: 'Puntos', value: 'puntos' },
+          { text: 'Votos', value: 'votos' },
+          { text: 'Comentarios', value: 'comentarios' },
+          { text: 'Ingresos', value: 'ingresos' },
+          { text: 'Ideas', value: 'ideas' },
+          { text: 'Retos', value: 'retos' }
+        ],
+       
+
     };
   },
 
