66e13a279f82de7de6bcc40fdf56f55cfefccd45
Yeison Caicedo <yecaicedo@unicauca.edu.co>
2019-12-04 19:26:00 -0500

    again

--- app/Blog.php
+++ app/Blog.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+                                                                                
+
 class Blog extends Model
 {
     protected $fillable = [
